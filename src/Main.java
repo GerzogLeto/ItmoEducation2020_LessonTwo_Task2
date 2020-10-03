85894e43ed97c3077ff84cd428f7dfba00aa3733
Anton Grudnov <stepfano@yandex.ru>
2020-10-03 22:42:17 +0300

    init commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1, answer = 2; i < 21; i++){
+            System.out.println(answer);
+            answer *=2;
+        }
+    }
+}
